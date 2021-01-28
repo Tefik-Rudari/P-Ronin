22983c512e8459f08bcc693384fd6a5674e0333a
unknown <dk42889@ubt-uni.net>
2021-01-28 16:04:15 +0100

    Chaning branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <>
       <Router>
-        {/* <Navbar /> */}
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
--- src/components/hero/HeroSection.js
+++ src/components/hero/HeroSection.js
@@ -21,14 +21,7 @@ function HeroSection() {
           GET STARTED
         </Button>
           </div>
-        {/* <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button> */}
+
       </div>
     </div>
   );
