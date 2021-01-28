aefffd6e4ef64e84b42dd68923678f1e733ef838
Tefik Rudari <tefikrudarii@gmail.com>
2021-01-28 16:30:27 +0100

    eslint shittttt

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Projects from './components/pages/Projects';
+import Product from './components/pages/Product';
 import SignUp from './components/pages/Signup';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/projects' component={Projects} />
+          <Route path='/product' component={Product} />
           <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
