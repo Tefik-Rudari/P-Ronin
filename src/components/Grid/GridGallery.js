8dd0ae644574f123db95d8ce6a749b67fe387d47
edonhoxha96 <edonhoxha39@gmail.com>
2021-02-05 15:42:42 +0100

    gridgallery update

--- src/components/Grid/GridGallery.js
+++ src/components/Grid/GridGallery.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import image1 from './img-8.jpg';
+import image1 from './decpro.PNG';
 import image2 from './img-9.jpg';
 // import Link from '@material-ui/core/Link';
 
@@ -14,28 +14,37 @@ import {
   ResponsiveContext,
 } from 'grommet';
 
+const customTheme = {
+  global: {
+    colors: {
+      focus: null
+    }
+  }
+};
 // const preventDefault = (event) => event.preventDefault();
 
 function GridGallery() {
   const size = useContext(ResponsiveContext);
   return (
-    <Grommet theme={grommet}>
+    <Grommet theme={customTheme}>
       <Box pad="medium">
         <Grid columns={size !== 'medium' ? 'medium' : '100%'} gap="small">
           <Card pad="small" >
-            <div class="container">
-            {/* <Anchor id="decpro" target="blank" href="http://decprojects.net/"> */}
-              <Image src={image1} fill="true" />
-            {/* </Anchor> */}
-            <Anchor id="decpro" target="blank" href="http://decprojects.net/">
-            <div class="overlay">
-              <div class="text">Decpro</div>
-            </div>
-            </Anchor>
+            <div className="kont">
+              {/* <Anchor className="kontinier" id="decpro" target="blank" href="http://decprojects.net/"> */}
+                <Image src={image1} fill="true" />
+              {/* </Anchor> */}
+              <Anchor id="decpro" target="blank" href="http://decprojects.net/">
+                <div className="overlay">
+                  <div className="text">DECPRO</div>
+                </div>
+              </Anchor>
             </div>
           </Card>
           <Card pad="small">
-            <img alt="img" src={image1} />
+            <Anchor id="decpro" target="blank" href="http://decprojects.net/">
+              <Image src={image1} fill="true" />
+            </Anchor>
           </Card>
           <Card pad="small">
             <img alt="img" src={image2} />
@@ -50,3 +59,14 @@ function GridGallery() {
 };
 
 export default GridGallery
+
+{/* <div className="container">
+<Anchor id="decpro" target="blank" href="http://decprojects.net/">
+<Image src={image1} fill="true" />
+</Anchor>
+<Anchor id="decpro" target="blank" href="http://decprojects.net/">
+<div className="overlay">
+<div className="text">Decpro</div>
+</div>
+</Anchor>
+</div> */}
