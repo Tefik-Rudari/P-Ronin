e445fa4b9c4e125138bdd7aa97fcd6f03f1bc6b8
edonhoxha96 <edonhoxha39@gmail.com>
2021-02-09 17:11:59 +0100

    WhyChooseUs improvements

--- src/components/pages/WhyChooseUs.js
+++ src/components/pages/WhyChooseUs.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./services.css"
-import { FcAcceptDatabase } from "react-icons/fc";
+import {GiPencilRuler} from "react-icons/gi";
+import {FiMail} from "react-icons/fi";
+import {ImBug} from "react-icons/im";
+import {FaTools} from "react-icons/fa"
 
 const WhyChooseUs = () => {
     return (
@@ -10,26 +13,35 @@ const WhyChooseUs = () => {
             <div className="maindiv">
                 <div className="topdiv">
                     <div className="top1">
-                        <div className="ikona"><FcAcceptDatabase size={28}/></div>
+                        <div className="ikona"><GiPencilRuler size={35} color="#EA6B66"/></div>
                         <div className="kontentdiv">
                             <p className="kontenttitle">Awesome Design</p>
-                            <p>ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg</p>
+                            <p className="kontenttext">ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg<br/>ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg</p>
                         </div>
                     </div>
                     <div className="top2">
-                    <div className="ikona"><i><FcAcceptDatabase /></i></div>
+                    <div className="ikona"><i><FiMail size={35} color="#65E5C4"/></i></div>
                         <div className="kontentdiv">
-                            <p className="kontenttitle">Awesome Design</p>
-                            <p>ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg</p>
+                            <p className="kontenttitle">Notification Update</p>
+                            <p className="kontenttext">ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg<br/>ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg</p>
                         </div>
                     </div>
                 </div>
-                <div className="bottomdiv">
-                    <div>
-                        <div className="ikona"><i></i></div>
-                        <div className="kontentdiv"></div>
+                <div className="topdiv">
+                    <div className="top1">
+                        <div className="ikona"><ImBug size={35} color="#8475CF"/></div>
+                        <div className="kontentdiv">
+                            <p className="kontenttitle">Bug Free code</p>
+                            <p className="kontenttext">ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg<br/>ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg</p>
+                        </div>
                     </div>
-                    <div></div>
+                    <div className="top2">
+                    <div className="ikona"><i><FaTools size={35} color="#2DADE8"/></i></div>
+                        <div className="kontentdiv">
+                            <p className="kontenttitle">Awesome Design</p>
+                            <p className="kontenttext">ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg<br/>ggfhjjkjksfjksdfjkdfsajkfajfsggffasfadfasggfgassg</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
