3a7f74ca6873d132deb2c4b5901953f4450cab5f
unknown <dk42889@ubt-uni.net>
2021-02-22 10:43:18 +0100

    commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,9 @@
 import '../../App.css';
 import HeroSection from '../hero/HeroSection.js';
-import ServicesDetails from '../Services/ServicesDetails.js';
 import WhyChooseUs from "../pages/WhyChooseUs"
 import GridGallery from '../Grid/GridGallery'
 import Footer from '../footer/Footer.js';
+import main_div from './main_div.css';
 
 
 function Home() {
@@ -11,7 +11,6 @@ function Home() {
     <div className='maindiv'>
       <HeroSection />
       <WhyChooseUs />
-      <ServicesDetails />
       <GridGallery />
       <Footer />
     </div>
