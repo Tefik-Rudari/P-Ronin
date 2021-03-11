00b9b8354f24d6a76dacaebb0873e65377d4e700
unknown <dk42889@ubt-uni.net>
2021-03-11 14:46:06 +0100

    navbar size for mobile fixed

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,11 +1,5 @@
 import '../../App.css';
 import Navigacionbar from '../Navbar/Navigacionbar'
-// import Gallery from 'react-photo-gallery'
-// import imazhi2 from '../Services/webpage.png';
-// import imazhi3 from '../Services/webservice.png';
-// import imazhi from '../Services/webhook.png';
-// import image1 from './img-8.jpg';
-// import image2 from './img-9.jpg';
 import './services.css'
  
 
